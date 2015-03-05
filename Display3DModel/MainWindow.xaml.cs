978263dfa05cff10d6af0d83575281b4b36ccd63
giaosucan <giaosucan1983@gmail.com>
2015-03-05 22:07:45 +0700

    3D proj

---
+++ Display3DModel/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+
+namespace Display3DModel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Path to the model file
+        private const string MODEL_PATH = "3DModel/File01.obj";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ModelVisual3D device3D = new ModelVisual3D();
+            device3D.Content = Display3d(MODEL_PATH);
+            // Add to view port
+            viewPort3d.Children.Add(device3D);
+        }
+
+        /// <summary>
+        /// Display 3D Model
+        /// </summary>
+        /// <param name="model">Path to the Model file</param>
+        /// <returns>3D Model Content</returns>
+        private Model3D Display3d(string model)
+        {
+            Model3D device = null;
+            try
+            {
+                //Adding a gesture here
+                viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+
+                //Import 3D model file
+                ModelImporter import = new ModelImporter();
+
+                //Load the 3D model file
+                device = import.Load(model);
+            }
+            catch (Exception e)
+            {
+                // Handle exception in case can not file 3D model
+                MessageBox.Show("Exception Error : " + e.StackTrace);
+            }
+            return device;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+There’re many famous software such as Maya, AutoCAD, Solid Work which can display the 3D model. Moreover, those software also support user to manipulate the 3D Model like Zoom, Move, Rotate the 3D model.
+
+This source code show you how to read a 3D model file, display it on Window screen as well as manipulate it (Zoom, Move, Rotate) like those software.
+
+We will develop a WPF Window Application (Window Presentation Framework) to display the 3D model file. The simple way to display 3D model is combining WPF with 3rd library (Helix Toolkit)
+http://helixtoolkit.codeplex.com/
